79d99e4f3cfa9d7ad38f1004293e8a65a7d108f3
Gerald0Juni0r <geraldo.gj63@gmail.com>
2021-11-07 00:06:51 -0300

    atualizações no 'index.php', 'recebeForm.php' e 'geral.css'

--- JS/index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('btn-confirmar').addEventListener('click', function(e){
-    e.preventDefault();
-    alert('Seja bem vindo ' + nome  )
-})
--- recebeForm.php
+++ recebeForm.php
@@ -1,18 +1,30 @@
 <!DOCTYPE html>
 <html lang="PT-BR">
-<body>
-    <?php 
-        echo "Nome: " . $_POST["nome"] . "<br>" ; 
-        echo "Sobrenome: " . $_POST["sobrenome"] . "<br>" ; 
-        echo "Email: " . $_POST["email"] . "<br>" ; 
-        echo "Senha: " . $_POST["senha"] . "<br>" ; 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        if($_POST["senha"] == $_POST["csenha"]){
-            echo "<br>Senha válida";
-        }
-        else{
-            echo "<br>Senha invalida";
-        }
-    ?>
+    <title>Cadastre-se</title>
+
+    <link rel="stylesheet" href="./CSS/geral.css">
+
+</head>
+<body>
+    <div id="recebeForm">
+        <?php 
+            echo "Nome: " . $_POST["nome"] . "<br>" ; 
+            echo "Sobrenome: " . $_POST["sobrenome"] . "<br>" ; 
+            echo "Email: " . $_POST["email"] . "<br>" ; 
+            echo "Senha: " . $_POST["senha"] . "<br>" ; 
+
+            if($_POST["senha"] == $_POST["csenha"]){
+                echo "<br> Senha válida";
+            }
+            else{
+                echo "<br> Senha invalida";
+            }
+        ?>
+    </div>
 <body>
 </html>
